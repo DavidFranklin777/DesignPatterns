e8470aef9f5941c9d32c4732295fad32ecfe6edb
DavidFranklin <davidfranklin777@gmail.com>
2022-03-23 22:31:44 +0100

    Singleton: Logger as an example using mutlithreading basics

--- main.cpp
+++ main.cpp
@@ -1,14 +1,35 @@
 #include <iostream>
 #include "Factory.h"
+#include "singleton.hpp"
 using namespace std;
+
+void user1log()
+{
+    // Even though we created logger1 and logger2, we see that only one instance of logger is created and that is used among
+    // every user who uses it
+    Logger* logger1  = Logger::getlogger();
+    logger1->log("User1");
+}
+
+void user2log()
+{
+    Logger* logger2  = Logger::getlogger();
+    logger2->log("User2");
+}
 
 int main()
 {
-    charName name = charNaruto;
-    character* pChar = nullptr;
-    pChar = factory::create(name);
-    pChar->printChar();
-    delete pChar;
-    cout << "Hello World!" << endl;
+//    charName name = charNaruto;
+//    character* pChar = nullptr;
+//    pChar = factory::create(name);
+//    pChar->printChar();
+//    delete pChar;
+//    cout << "Hello World!" << endl;
+
+    thread t1(user1log);
+    thread t2(user2log);
+
+    t1.join();
+    t2.join();
     return 0;
 }
---
+++ singleton.hpp
@@ -0,0 +1,60 @@
+#ifndef SINGLETON_HPP
+#define SINGLETON_HPP
+
+#include <iostream>
+#include <thread>
+#include <mutex>
+
+
+using namespace std;
+
+class Logger
+{
+public:
+    // We need a static function, because we are returning a static pointer to an object
+    static Logger* getlogger()
+    {
+        if(loggerInstance == nullptr)
+       {    // lock the instance creation via mutex, only if the loggerinstance is null
+            mtx.lock();
+            // Only create an instance of logger when it is null, otherwise just return the previous instance
+            // Since loggerInstance is a static variable, its scope ends only at the end of the program
+            // That means, other users can use the same loggerInstance
+            if (loggerInstance == nullptr)
+            {
+                loggerInstance = new Logger();
+            }
+            mtx.unlock();
+        }
+        return loggerInstance;
+    }
+
+    void log(string msg)
+    {
+        cout << msg << endl;
+    }
+
+private:
+    static mutex mtx;
+    static Logger* loggerInstance;
+    // ctr - counter to test how many instances are being created
+    // static because we dont want it to reset
+    static int     ctr;
+    Logger()
+    {
+       // If ctr is more than one, our singleton has failed
+       // cout to test how many instances are created
+       ctr++;
+       cout << "New instance created " << ctr << endl;
+    }
+
+};
+
+
+// Static variables must be instantiated/initialized outside of class,
+// since they are not bound to any class to be initialized inside them
+Logger* Logger::loggerInstance = nullptr;
+int Logger::ctr = 0;
+mutex Logger::mtx;
+
+#endif // SINGLETON_HPP
